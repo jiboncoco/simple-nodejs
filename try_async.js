23dba1b3a307783221ab2b1f05d76ffee25cff2e
Art By Code <artbycode.dev@gmail.com>
2021-07-27 00:52:16 +0700

    modify code add time in async

--- try_async.js
+++ try_async.js
@@ -1,10 +1,18 @@
 // asynchronous
 
+// const getUserSync = (id, callback) => {
+//     setTimeout(() => {
+//         const flight = id === 1 ? 'Garuda Indonesia' : 'Lion Air';
+//         callback({id, flight});
+//     }, 1000)
+// };
+
 const getUserSync = (id, callback) => {
+    const time = id === 1 ? 3000 : 2000;
     setTimeout(() => {
         const flight = id === 1 ? 'Garuda Indonesia' : 'Lion Air';
         callback({id, flight});
-    }, 1000)
+    }, time)
 };
 
 const flightOne = getUserSync(1, (result) => {
